33859e13000ea006f51af116629053c97103378b
unknown <krishnacheashty@gmail.com>
2023-06-09 14:14:59 +0600

    [DONE].evn and express,mongo,cors setup

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application } from "express";
+import cors from 'cors';
+
+
+const app:Application=express();
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.get("/",(req,res)=>{
+    res.send("hello krishna")
+})
+
+
+export default app;
+
+
